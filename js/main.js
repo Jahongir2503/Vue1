599eeceb2285bb6a96f8298ba08344194d6d721e
mirzo <jojo.isiomidinov@gmail.com>
2024-01-29 15:16:20 +0500

    5 практическая

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 let app = new Vue({
     el: '#app',
-
-    data: {
-        product: "Socks",
-        image: "./js/assets/vmSocks-blue-onWhite.jpg",
-        altText: "A pair of socks",
-        inStock: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green'
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue'
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        turnItDown() {
+            this.cart -=1
+        }
+        },
+        data: {
+            product: "Socks",
+            image: "./assets/vmSocks-green-onWhite.jpg",
+            altText: "A pair of socks",
+            inStock: true,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                }
+            ],
+            cart: 0,
+            updateProduct(variantImage) {
+                this.image = variantImage
             }
-        ]
-    }
+
+
+        }
+
 })
+
 
 
