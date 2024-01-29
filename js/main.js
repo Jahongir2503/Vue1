4ffdc1203ca9adb33b9ab34d3fdc48b0cb684e1f
mirzo <jojo.isiomidinov@gmail.com>
2024-01-29 14:33:01 +0500

    3 практическая

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 let app = new Vue({
     el: '#app',
+
     data: {
         product: "Socks",
         image: "./js/assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        inventory: 0,
         inStock: true,
-        onSale: "On Sale",
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 })
+
+
